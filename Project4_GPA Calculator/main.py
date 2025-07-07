5465d3df919704f9e9427a0adeb2b88e5dbd8a0f
Febin Sebastian <febin.vadakkan@gmail.com>
2025-07-07 10:32:02 +0200

    Commiting latest addition of Seminar Electromobility grade and credits

--- Project4_GPA Calculator/main.py
+++ Project4_GPA Calculator/main.py
@@ -6,7 +6,8 @@ transcript_febin = {'Vehicle Vibrations' : [1, 3], 'Drives and Gears' : [1.3, 3]
               'Foundations of Software Engineering' : [2.3, 4], 'Safety and Reliability of Embedded Systems' : [3.3, 4],
               'Automotive Software and Systems Engineering' : [3.7, 4], 'CVT Programming Project' : [1, 4],
               'Principles of Electrical and Computer Engineering' : [3.7, 5],'Introduction to Autonomous Systems' : [1.7, 4],
-              'Vehicle Propulsion Systems' : [2.7, 3], 'Simulation of Bus Systems' : [2, 4], 'Electric and Hybrid vehicles' : [1.7, 3], 'CAE in Control Engineering' : [2.3, 4],
+              'Vehicle Propulsion Systems' : [2.7, 3], 'Simulation of Bus Systems' : [2, 4], 'Electric and Hybrid vehicles' : [1.7, 3],
+              'CAE in Control Engineering' : [2.3, 4], 'Seminar Electromobility' : [0,3],
               'Industrial Organisation' : [2.7, 4.5], 'Linguistics and Language Processing' : [1.7, 5]}
 
 sumOfNumerator = 0
