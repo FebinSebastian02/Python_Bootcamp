34d21308bd0be423d94d00d508870393eccb8c9e
Febin Sebastian <febin.vadakkan@gmail.com>
2025-07-31 13:49:15 +0200

    Commiting dictionaries basics file

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+travel_log = {
+    "Germany": {
+        "cities visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total visits": 5
+    }
+}
+
+print(travel_log["Germany"]["cities visited"][2])
+
+nested_list = ["A", "B", ["C", "D"]]
+print(nested_list[2][1])
