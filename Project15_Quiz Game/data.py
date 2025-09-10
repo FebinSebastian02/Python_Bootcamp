193dc39d4b9ded42c9cffed7b86c9f4131d45be9
Febin Sebastian <febin.vadakkan@gmail.com>
2025-09-10 17:38:47 +0200

    Commiting Project15 quiz game from day 17

---
+++ Project15_Quiz Game/data.py
@@ -0,0 +1,5 @@
+question_data = [
+    {"text": "1 + 2 = 3", "answer": "true"},
+    {"text": "Apple is a vegetable", "answer": "false"},
+    {"text": "Ronaldo has 5 Ballon D' ors", "answer": "true"}
+]
